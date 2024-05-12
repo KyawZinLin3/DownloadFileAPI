c73e2a6933cfa9214a05af8bfa7b1323891c9c6c
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-05-12 23:23:45 +0630

    Add project files.

---
+++ DownloadFileAPI/Controllers/DownloadFileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace DownloadFileAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DownloadFileController : Controller
+    {
+        private readonly ILogger<DownloadFileController> _logger;
+
+        public DownloadFileController(ILogger<DownloadFileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("download")]
+        public IActionResult Download()
+        {
+            var filepath = Path.Combine("images", "Image1.png");
+            return File(System.IO.File.ReadAllBytes(filepath), "image/png", System.IO.Path.GetFileName(filepath));
+        }
+    }
+}
+
